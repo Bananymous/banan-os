ac2ba8bc562dfc36042793711eb58021c4d3f637
Bananymous <bananymousosq@gmail.com>
2022-12-10 00:30:00 +0200

    LibC: Remove actual printing from putc() since we were panicing anyway

--- libc/stdio/putchar.cpp
+++ libc/stdio/putchar.cpp
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
 #if defined(__is_libk)
-#include <kernel/tty.h>
-#include <kernel/panic.h>
+	#include <kernel/panic.h>
 #else
-#include <stdlib.h>
+	#include <stdlib.h>
 #endif
 
 int putchar(int c)
 {
 #if defined(__is_libk)
 	Kernel::panic("Please use kprint() instead of stdio");
-	char ch = (char)c;
-	terminal_write(&ch, sizeof(ch));
 #else
 	abort();
 #endif
