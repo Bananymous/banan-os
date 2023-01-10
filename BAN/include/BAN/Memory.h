85068e27d6c7b5b5430e31c3509741679264edc1
Bananymous <bananymousosq@gmail.com>
2023-01-10 17:51:33 +0200

    BAN: BAN/Memory.h now provides the operator new and delete

--- BAN/include/BAN/Memory.h
+++ BAN/include/BAN/Memory.h
@@ -15,4 +15,32 @@ namespace BAN
 		static constexpr auto& allocator = malloc;
 		static constexpr auto& deallocator = free;
 	#endif
+
+	template<typename T>
+	class OwnPtr
+	{
+	public:
+		template<typename... Args>
+		OwnPtr(const Args&... args)
+		{
+			m_pointer = new T(args...);
+		}
+
+		~OwnPtr()
+		{
+			delete m_pointer;
+		}
+
+	private:
+		T* m_pointer = nullptr;
+	};
+
 }
+
+inline void* operator new(size_t size)		{ return BAN::allocator(size); }
+inline void* operator new[](size_t size)	{ return BAN::allocator(size); }
+
+inline void operator delete(void* addr)				{ BAN::deallocator(addr); }
+inline void operator delete[](void* addr)			{ BAN::deallocator(addr); }
+inline void operator delete(void* addr, size_t)		{ BAN::deallocator(addr); }
+inline void operator delete[](void* addr, size_t)	{ BAN::deallocator(addr); }
