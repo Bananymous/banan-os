fd16c1cf58b0b1015e2d539a4e7a6d73b79c788f
Bananymous <bananymousosq@gmail.com>
2023-01-16 19:13:11 +0200

    LibC: Add is*() functions to libc

---
+++ libc/include/ctype.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+int isalnum(int);
+int isalpha(int);
+int isascii(int);
+int isblank(int);
+int iscntrl(int);
+int isdigit(int);
+int isgraph(int);
+int islower(int);
+int isprint(int);
+int ispunct(int);
+int isspace(int);
+int isupper(int);
+int isxdigit(int);
+
+__END_DECLS
