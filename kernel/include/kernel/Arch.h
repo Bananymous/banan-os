777ede328e5f4237d5c274959c4124073430ac1f
Bananymous <bananymousosq@gmail.com>
2023-02-02 23:19:44 +0200

    Kernel: Add helper functions for reading current rsp and rbp

--- kernel/include/kernel/Arch.h
+++ kernel/include/kernel/Arch.h
@@ -5,3 +5,11 @@
 #if !defined(__arch) || (__arch != x86_64 && __arch != i386)
 	#error "Unsupported architecture"
 #endif
+
+#if ARCH(x86_64)
+	#define read_rsp(rsp) asm volatile("movq %%rsp, %0" : "=r"(rsp))
+	#define read_rbp(rbp) asm volatile("movq %%rbp, %0" : "=r"(rbp))
+#else
+	#define read_rsp(rsp) asm volatile("movl %%esp, %0" : "=r"(rsp))
+	#define read_rbp(rbp) asm volatile("movl %%ebp, %0" : "=r"(rbp))
+#endif
