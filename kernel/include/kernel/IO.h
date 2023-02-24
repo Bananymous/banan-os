34f9912a1dfc1726a105e50c99ca9476f4095e2b
Bananymous <bananymousosq@gmail.com>
2023-02-24 12:39:38 +0200

    Kernel: Add IO functions inl, outl, and ins{b,w,l} to read into buffer

--- kernel/include/kernel/IO.h
+++ kernel/include/kernel/IO.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 
 namespace IO
 {
@@ -27,6 +28,33 @@ namespace IO
 		uint16_t ret;
 		asm volatile("inw %1, %0" : "=a"(ret) : "Nd"(port));
 		return ret;
+	}
+
+	static inline void outl(uint16_t port, uint32_t val)
+	{
+		asm volatile("outl %0, %1" : : "a"(val), "Nd"(port));
+	}
+
+	static inline uint32_t inl(uint16_t port)
+	{
+		uint32_t ret;
+		asm volatile("inl %1, %0" : "=a"(ret) : "Nd"(port));
+		return ret;
+	}
+
+	static inline void insb(uint16_t port, uint8_t* buffer, size_t count)
+	{
+		asm volatile("rep insb" : "+D"(buffer), "+c"(count), "=m"(*buffer) : "Nd"(port) : "memory");
+	}
+
+	static inline void insw(uint16_t port, uint16_t* buffer, size_t count)
+	{
+		asm volatile("rep insw" : "+D"(buffer), "+c"(count), "=m"(*buffer) : "Nd"(port) : "memory");
+	}
+
+	static inline void insl(uint16_t port, uint32_t* buffer, size_t count)
+	{
+		asm volatile("rep insl" : "+D"(buffer), "+c"(count), "=m"(*buffer) : "Nd"(port) : "memory");
 	}
 
 	static inline void io_wait()
