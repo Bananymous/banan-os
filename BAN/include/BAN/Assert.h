94395821541c34632334df7a683d953ff8c767e1
Bananymous <bananymousosq@gmail.com>
2023-02-26 02:53:58 +0200

    BAN: Add ASSERT_NOT_REACHED()

--- BAN/include/BAN/Assert.h
+++ BAN/include/BAN/Assert.h
@@ -3,6 +3,7 @@
 #if defined(__is_kernel)
 	#include <kernel/Panic.h>
 	#define ASSERT(cond) do { if (!(cond)) Kernel::panic("ASSERT("#cond") failed"); } while(false)
+	#define ASSERT_NOT_REACHED() Kernel::panic("ASSERT_NOT_REACHED() failed")
 #else
 	#error "NOT IMPLEMENTED"
 #endif
