10ef3c6a5403ec2db6f9cc1f0cc9b42d924b74c0
Bananymous <bananymousosq@gmail.com>
2023-03-04 02:59:48 +0200

    LibC: add declarations for malloc and free

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <sys/cdefs.h>
+#include <stddef.h>
 
 __BEGIN_DECLS
 
 __attribute__((__noreturn__))
 void abort(void);
+
+void* malloc(size_t);
+void free(void*);
 
 __END_DECLS
