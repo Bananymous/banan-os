e49d3c7bfe12fcde260fffbc3441e9572dc03950
Bananymous <bananymousosq@gmail.com>
2023-04-02 04:07:27 +0300

    BuildSystem: We are now using cmake instead of plain make
    I have been annoyed for a while since I had to build everything
    when running the os since the buildsystem was really bad.
    
    I wanted to rewrite the whole build system and changed to using cmake
    
    :)

---
+++ image.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+if [ ! -f $DISK_IMAGE_PATH ]; then
+	$(dirname "$0")/image-full.sh
+	exit 0
+fi
+
+MOUNT_DIR=/mnt
+
+LOOP_DEV=$(sudo losetup -f --show $DISK_IMAGE_PATH)
+sudo partprobe $LOOP_DEV
+
+ROOT_PARTITON=${LOOP_DEV}p2
+
+sudo mount $ROOT_PARTITON $MOUNT_DIR
+
+sudo cp -r ${SYSROOT}/* ${MOUNT_DIR}/
+
+sudo umount $MOUNT_DIR
+
+sudo losetup -d $LOOP_DEV
--- qemu.sh
+++ qemu.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
-. ./disk.sh
  
-qemu-system-$(./target-triplet-to-arch.sh $HOST)		\
-	-m 128												\
-	-smp 2												\
-	-drive format=raw,media=disk,file=banan-os.img		\
-	-serial stdio										\
-	-accel kvm											\
- 
+qemu-system-$BANAN_ARCH										\
+	-m 128													\
+	-smp 2													\
+	-drive format=raw,media=disk,file=${DISK_IMAGE_PATH}	\
+	-serial stdio											\
+	-accel kvm												\
