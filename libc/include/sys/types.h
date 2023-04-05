12351d5cb6d7bf062e7ddd3841705ae85765987c
Bananymous <bananymousosq@gmail.com>
2023-04-05 15:03:24 +0300

    LibC: sys/types uses 'typedef' instead of 'using'

--- libc/include/sys/types.h
+++ libc/include/sys/types.h
@@ -5,18 +5,17 @@
 
 __BEGIN_DECLS
 
-
-using blkcnt_t = int32_t;
-using blksize_t = int32_t;
-using dev_t = uint64_t;
-using ino_t = uint32_t;
-using mode_t = uint16_t;
-using nlink_t = uint32_t;
-using off_t = int64_t;
-using time_t = uint64_t;
-using id_t = int32_t;
-using pid_t = id_t;
-using uid_t = id_t;
-using gid_t = id_t;
+typedef int32_t		blkcnt_t;
+typedef int32_t		blksize_t;
+typedef uint64_t	dev_t;
+typedef uint32_t	ino_t;
+typedef uint16_t	mode_t;
+typedef uint32_t	nlink_t;
+typedef int64_t		off_t;
+typedef uint64_t	time_t;
+typedef int32_t		id_t;
+typedef id_t		pid_t;
+typedef id_t		uid_t;
+typedef id_t		gid_t;
 
 __END_DECLS
