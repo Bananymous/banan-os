e704968f96436b460590679d2143f932cbfe9565
Bananymous <bananymousosq@gmail.com>
2023-04-09 22:18:12 +0300

    Kernel: Threads can now be terminated mid execution

--- kernel/include/kernel/Thread.h
+++ kernel/include/kernel/Thread.h
@@ -18,6 +18,13 @@ namespace Kernel
 	public:
 		using entry_t = void(*)(void*);
 
+		enum class State
+		{
+			NotStarted,
+			Executing,
+			Terminating,
+		};
+
 	public:
 		static BAN::ErrorOr<Thread*> create(entry_t, void* = nullptr, BAN::RefPtr<Process> = nullptr);
 		~Thread();
@@ -29,8 +36,9 @@ namespace Kernel
 		uintptr_t rsp() const { return m_rsp; }
 		uintptr_t rip() const { return m_rip; }
 
-		void set_started() { m_started = true; }
-		bool started() const { return m_started; }
+		void set_started() { ASSERT(m_state == State::NotStarted); m_state = State::Executing; }
+		State state() const { return m_state; }
+		void terminate() { m_state = State::Terminating; }
 
 		static Thread& current() ;
 		BAN::RefPtr<Process> process();
@@ -46,8 +54,10 @@ namespace Kernel
 		uintptr_t	m_rip			= 0;
 		uintptr_t	m_rsp			= 0;
 		const pid_t	m_tid			= 0;
-		bool		m_started		= false;
+		State		m_state { State::NotStarted };
 		BAN::RefPtr<Process> m_process;
+
+		friend class Scheduler;
 	};
 	
 }
