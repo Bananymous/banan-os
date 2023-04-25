c20ba3064d2d3086fdde119ef9ba304924164f6f
Bananymous <bananymousosq@gmail.com>
2023-04-25 14:50:26 +0300

    Userspace: Simple stdio test

--- userspace/test.c
+++ userspace/test.c
@@ -2,7 +2,32 @@
 
 int main()
 {
-	if (printf("Hello %s!", "World") == -1)
-		perror(NULL);
+	FILE* fp = fopen("/boot/grub/grub.cfg", "r");
+	if (fp == NULL)
+	{
+		perror("fopen");
+		return 1;
+	}
+
+	for (;;)
+	{
+		char buffer[128];
+		size_t nread = fread(buffer, 1, sizeof(buffer) - 1, fp);
+		if (nread == 0)
+		{
+			if (ferror(fp))
+				perror("fread");
+			break;
+		}
+		buffer[nread] = '\0';
+		fputs(buffer, stdout);
+	}
+
+	if (fclose(fp) == EOF)
+	{
+		perror("fclose");
+		return 1;
+	}
+
 	return 0;
 }
