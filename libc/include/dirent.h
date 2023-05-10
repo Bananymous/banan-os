1d4a6c3a428ddb58b2a0f1e2a79a0810ae1f4c20
Bananymous <bananymousosq@gmail.com>
2023-05-11 01:42:52 +0300

    LibC: add function declarations to dirent.h

--- libc/include/dirent.h
+++ libc/include/dirent.h
@@ -12,4 +12,16 @@ struct dirent
 	char d_name[];
 };
 
+int				alphasort(const struct dirent**, const struct dirent**);
+int				closedir(DIR*);
+int				dirfd(DIR*);
+DIR*			fdopendir(int);
+DIR*			opendir(const char*);
+struct dirent*	readdir(DIR*);
+int				readdir_r(DIR*, struct dirent*, struct dirent**);
+void			rewinddir(DIR*);
+int				scandir(const char*, struct dirent***, int (*)(const struct dirent*), int (*)(const struct dirent**, const struct dirent**));
+void			seekdir(DIR*, long);
+long			telldir(DIR*);
+
 __END_DECLS
