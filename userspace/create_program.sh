82dcec9576506c60ddb9fa60d3a5b5473e47c4f0
Bananymous <bananymousosq@gmail.com>
2023-05-16 19:21:07 +0300

    Buildsystem: Fix userspace link order

--- userspace/create_program.sh
+++ userspace/create_program.sh
@@ -18,7 +18,8 @@ set(SOURCES
 add_executable($PROGRAM_NAME \${SOURCES})
 target_compile_options($PROGRAM_NAME PUBLIC -O2 -g)
 add_dependencies($PROGRAM_NAME libc-install)
-target_link_options($PROGRAM_NAME PUBLIC -nodefaultlibs -lc)
+target_link_options($PROGRAM_NAME PUBLIC -nodefaultlibs)
+target_link_libraries($PROGRAM_NAME PUBLIC \${BANAN_LIB}/libc.a)
 
 add_custom_target($PROGRAM_NAME-install
 	COMMAND cp \${CMAKE_CURRENT_BINARY_DIR}/$PROGRAM_NAME \${BANAN_BIN}/
