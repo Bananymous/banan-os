67886b0c5a267d4d1cf45df4785b4e06eead9c27
Bananymous <bananymousosq@gmail.com>
2023-05-30 23:57:03 +0300

    Kernel: Move V2P and P2V to Memory/Types.h

--- kernel/include/kernel/Memory/Types.h
+++ kernel/include/kernel/Memory/Types.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <kernel/Arch.h>
+
+#if ARCH(x86_64)
+	#define KERNEL_OFFSET 0xFFFFFFFF80000000
+	#define V2P(vaddr) (((vaddr_t)(vaddr)) - KERNEL_OFFSET)
+	#define P2V(paddr) (((paddr_t)(paddr)) + KERNEL_OFFSET)
+#else
+	#error
+#endif
+
 #define PAGE_SIZE 4096
 #define PAGE_FLAG_MASK ((uintptr_t)0xFFF)
 #define PAGE_ADDR_MASK (~PAGE_FLAG_MASK)
