d0a0e3bdef4aeefd41675790a20d6afe6a9f26e9
Bananymous <bananymousosq@gmail.com>
2023-06-12 20:36:16 +0300

    Userspace: Add u8sum
    This program caluculates the sum of bytes in file mod 256

---
+++ userspace/u8sum/main.cpp
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		FILE* fp = fopen(argv[i], "r");
+		if (fp == nullptr)
+		{
+			perror("nullptr");
+			continue;
+		}
+
+		uint8_t sum = 0;
+
+		uint8_t buffer[1024];
+		while (size_t ret = fread(buffer, 1, sizeof(buffer), fp))
+			for (int j = 0; j < ret; j++)
+				sum += buffer[j];
+
+		if (ferror(fp))
+			perror("fread");
+		else
+			printf("%s: %02x\n", argv[i], sum);
+
+		fclose(fp);
+	}
+}
