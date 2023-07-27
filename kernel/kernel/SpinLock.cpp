6c0f864a6ea3a3e4a755cdb6afe03980c9848858
Bananymous <bananymousosq@gmail.com>
2023-07-27 18:36:44 +0300

    Kernel: Locks allow locking after locker is invalid
    SpinLock and RecursiveSpinLock will now allow locking after the initial
    locker is invalid. This allows us to kill threads even if they are holding
    internal locks

--- kernel/kernel/SpinLock.cpp
+++ kernel/kernel/SpinLock.cpp
@@ -1,24 +1,39 @@
 #include <kernel/Scheduler.h>
 #include <kernel/SpinLock.h>
+#include <kernel/CriticalScope.h>
 
 namespace Kernel
 {
 
 	void SpinLock::lock()
 	{
-		while (__sync_lock_test_and_set(&m_lock, 1))
-			while (m_lock)
-				Scheduler::get().reschedule();
+		pid_t tid = Scheduler::current_tid();
+		while (true)
+		{
+			{
+				CriticalScope _;
+				ASSERT(m_locker != tid);
+				if (m_locker == -1 || !Scheduler::is_valid_tid(m_locker))
+				{
+					m_locker = tid;
+					break;
+				}
+			}
+			Scheduler::get().reschedule();
+		}
 	}
 
 	void SpinLock::unlock()
 	{
-		__sync_lock_release(&m_lock);
+		CriticalScope _;
+		ASSERT(m_locker == Scheduler::current_tid());
+		m_locker = -1;
 	}
 
 	bool SpinLock::is_locked() const
 	{
-		return m_lock;
+		CriticalScope _;
+		return m_locker != -1;
 	}
 
 	void RecursiveSpinLock::lock()
@@ -33,7 +48,7 @@ namespace Kernel
 				m_lock_depth++;
 				break;
 			}
-			if (m_locker == -1)
+			if (m_locker == -1 || !Scheduler::is_valid_tid(m_locker))
 			{
 				m_locker = tid;
 				m_lock_depth = 1;
