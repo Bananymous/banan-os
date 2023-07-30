08cdf8858673e61415d337233c5da3c1b64281c0
Bananymous <bananymousosq@gmail.com>
2023-07-30 15:58:35 +0300

    Kernel: Cleanup signal trampoline

--- kernel/kernel/Signal.cpp
+++
@@ -1,8 +0,0 @@
-#include <sys/syscall.h>
-#include <kernel/Syscall.h>
-
-extern "C" __attribute__((section(".userspace")))
-void signal_done(int signal)
-{
-	Kernel::syscall(SYS_SIGNAL_DONE, signal);
-}
