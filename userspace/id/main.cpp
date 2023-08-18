30463c9082908a284b9c283dc964301f1c2484e5
Bananymous <bananymousosq@gmail.com>
2023-08-18 15:36:51 +0300

    Id: Print username corresponding to uid/euid
    Also print euid and egid if they differ from uid or gid.

--- userspace/id/main.cpp
+++ userspace/id/main.cpp
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pwd.h>
 
 int main()
 {
-	printf("uid %u, gid %u, euid %u, egid %u\n", getuid(), getgid(), geteuid(), getegid());
+	uid_t uid = getuid();
+	uid_t euid = geteuid();
+
+	gid_t gid = getgid();
+	gid_t egid = getegid();
+
+	passwd* pw_uid = getpwuid(uid);
+	if (pw_uid == nullptr)
+	{
+		fprintf(stderr, "Unknown user #%d\n", uid);
+		return 1;
+	}
+
+	passwd* pw_euid = getpwuid(euid);
+	if (pw_euid == nullptr)
+	{
+		fprintf(stderr, "Unknown user #%d\n", euid);
+		return 1;
+	}
+
+	printf("uid=%u(%s)", uid, pw_uid->pw_name);
+	if (uid != euid)
+		printf(",euid=%u(%s)",euid, pw_euid->pw_name);
+
+	printf(" gid=%u", gid);
+	if (gid != egid)
+		printf(",egid=%u", egid);
+
+	printf("\n");
 }
