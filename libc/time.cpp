a15ffcb0713e0f6a58fcdc83c6516c76b0f77992
Bananymous <bananymousosq@gmail.com>
2023-09-04 13:52:58 +0300

    LibC: add time() implementation

--- libc/time.cpp
+++ libc/time.cpp
@@ -11,3 +11,13 @@ int nanosleep(const struct timespec* rqtp, struct timespec* rmtp)
 {
 	return syscall(SYS_NANOSLEEP, rqtp, rmtp);
 }
+
+time_t time(time_t* tloc)
+{
+	timespec tp;
+	if (clock_gettime(CLOCK_REALTIME, &tp) == -1)
+		return -1;
+	if (tloc)
+		*tloc = tp.tv_sec;
+	return tp.tv_sec;
+}
