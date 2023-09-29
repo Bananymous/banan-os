b57811b371510ad9e7e06dd821826e55eabd2a23
Bananymous <bananymousosq@gmail.com>
2023-09-29 17:24:55 +0300

    Userspace: Implement cat-mmap
    This behaves exactly as cat, but uses mmap to read the file.

---
+++ userspace/cat-mmap/main.cpp
@@ -0,0 +1,62 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+bool cat_file(int fd)
+{
+	struct stat st;
+	if (fstat(fd, &st) == -1)
+	{
+		perror("stat");
+		return false;
+	}
+
+	void* addr = mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (addr == MAP_FAILED)
+	{
+		perror("mmap");
+		return false;
+	}
+
+	ssize_t nwrite = write(STDOUT_FILENO, addr, st.st_size);
+	if (nwrite == -1)
+		perror("write");
+
+	if (munmap(addr, st.st_size) == -1)
+	{
+		perror("munmap");
+		return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char** argv)
+{
+	int ret = 0;
+
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			int fd = open(argv[i], O_RDONLY);
+			if (fd == -1)
+			{
+				perror(argv[i]);
+				ret = 1;
+				continue;
+			}
+			if (!cat_file(fd))
+				ret = 1;
+			close(fd);
+		}
+	}
+	else
+	{
+		if (!cat_file(STDIN_FILENO))
+			ret = 1;
+	}
+
+	return ret;
+}
