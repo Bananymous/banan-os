a63006afaffaa61fc897cd697d0d6f0bf4dcca84
Bananymous <oskari.alaranta@bananymous.com>
2023-10-30 11:11:10 +0200

    Userspace: Add quick test for global ctors and dtors

---
+++ userspace/test-globals/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct foo_t
+{
+	foo_t() { printf("global constructor works\n"); }
+	~foo_t() { printf("global destructor works\n"); }
+};
+
+foo_t foo;
+
+int main()
+{
+
+}
