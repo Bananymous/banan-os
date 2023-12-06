06a84da8446d8088de518f266f068445a05c8635
Bananymous <oskari.alaranta@bananymous.com>
2023-12-06 18:15:42 +0200

    Userspace: Implement barebones sudo
    This doesn't do any password checking or anything. Just sets uid and
    gid before execvp()

---
+++ userspace/sudo/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int usage(char* argv0, int ret)
+{
+	FILE* fp = (ret == 0) ? stdout : stderr;
+	fprintf(fp, "usage: %s COMMAND [ARGUMENTS]\n", argv0);
+	return ret;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+		return usage(argv[0], 1);
+	
+	if (setuid(0) == -1)
+	{
+		perror("setuid");
+		return 1;
+	}
+	if (setgid(0) == -1)
+	{
+		perror("setgid");
+		return 1;
+	}
+
+	execvp(argv[1], argv + 1);
+
+	perror("execv");
+	return 1;
+}
