336daa2cc59d0646a708cbbd4b622d923e0a1638
Bananymous <oskari.alaranta@bananymous.com>
2023-12-06 12:57:13 +0200

    Kernel: Add helper for checking whether segment is from userspace

--- kernel/include/kernel/GDT.h
+++ kernel/include/kernel/GDT.h
@@ -5,6 +5,11 @@
 namespace Kernel::GDT
 {
 
+	static constexpr inline bool is_user_segment(uint8_t segment)
+	{
+		return (segment & 3) == 3;
+	}
+
 	void initialize();
 	void set_tss_stack(uintptr_t);
 
