2faf90bc2b83a7d405cbff802eed801d74dd5baa
Bananymous <oskari.alaranta@bananymous.com>
2023-12-23 18:46:14 +0200

    AOC2023: Add script to create day template and download input

---
+++ userspace/aoc2023/day-template/main.cpp
@@ -0,0 +1,48 @@
+#include <ctype.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+i64 puzzle1(FILE* fp)
+{
+	(void)fp;
+	return -1;
+}
+
+i64 puzzle2(FILE* fp)
+{
+	(void)fp;
+	return -1;
+}
+
+int main(int argc, char** argv)
+{
+	const char* file_path = "/usr/share/aoc2023/day-template_input.txt";
+
+	if (argc >= 2)
+		file_path = argv[1];
+
+	FILE* fp = fopen(file_path, "r");
+	if (fp == nullptr)
+	{
+		perror("fopen");
+		return 1;
+	}
+
+	printf("puzzle1: %" PRId64 "\n", puzzle1(fp));
+
+	fseek(fp, 0, SEEK_SET);
+
+	printf("puzzle2: %" PRId64 "\n", puzzle2(fp));
+
+	fclose(fp);
+}
--- userspace/aoc2023/input/get-input.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget --no-cookies --header "Cookie: session=$AOC_SESSION" https://adventofcode.com/2023/day/$1/input -O day$1_input.txt
