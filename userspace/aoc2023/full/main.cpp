762d575d70bcb56bbd10ebab93816c62c360c1c5
Bananymous <oskari.alaranta@bananymous.com>
2023-12-23 18:45:40 +0200

    AOC2023: Add program to run all days

---
+++ userspace/aoc2023/full/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	for (int i = 1; i <= 16; i++)
+	{
+		printf("day %d:\n", i);
+
+		char command[128];
+		sprintf(command, "/bin/aoc2023/day%d", i);
+
+		system(command);
+	}
+
+	return 0;
+}
