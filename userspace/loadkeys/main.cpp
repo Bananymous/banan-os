64a63fa4be2035bde3f6780285af33554e9705cd
Bananymous <oskari.alaranta@bananymous.com>
2024-01-10 14:57:50 +0200

    Userspace: Add loadkeys program to change keymap

---
+++ userspace/loadkeys/main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/banan-os.h>
+#include <sys/stat.h>
+
+int try_load_keymap(const char* path)
+{
+	if (load_keymap(path) == -1)
+	{
+		perror("load_keymap");
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "usage: %s KEYMAP\n", argv[0]);
+		return 1;
+	}
+
+	struct stat st;
+	if (stat(argv[1], &st) == 0)
+		return try_load_keymap(argv[1]);
+	
+	char buffer[128];
+	strcpy(buffer, "/usr/share/keymaps/");
+	strcat(buffer, argv[1]);
+
+	if (stat(buffer, &st) == 0)
+		return try_load_keymap(buffer);
+
+	strcat(buffer, ".keymap");
+
+	if (stat(buffer, &st) == 0)
+		return try_load_keymap(buffer);
+
+	fprintf(stderr, "Keymap '%s' not found\n", argv[1]);
+	return 1;
+}
