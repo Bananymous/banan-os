d452cf4170212ab912ee19e2179d1b0a70fac189
Bananymous <oskari.alaranta@bananymous.com>
2024-02-09 01:20:40 +0200

    Kernel: Fix checksum for packets with odd number of bytes

--- kernel/include/kernel/Networking/NetworkLayer.h
+++ kernel/include/kernel/Networking/NetworkLayer.h
@@ -38,6 +38,8 @@ namespace Kernel
 			checksum += BAN::host_to_network_endian(reinterpret_cast<const uint16_t*>(&pseudo_header)[i]);
 		for (size_t i = 0; i < packet.size() / sizeof(uint16_t); i++)
 			checksum += BAN::host_to_network_endian(reinterpret_cast<const uint16_t*>(packet.data())[i]);
+		if (packet.size() % 2)
+			checksum += (uint16_t)packet[packet.size() - 1] << 8;
 		while (checksum >> 16)
 			checksum = (checksum >> 16) + (checksum & 0xFFFF);
 		return ~(uint16_t)checksum;
