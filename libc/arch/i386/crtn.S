7d1b7436d4c294f7ce0191291040e31160cceed9
Bananymous <oskari.alaranta@bananymous.com>
2024-03-21 15:20:20 +0200

    LibC: Dummy ctr* files for i386
    This allows compilation of libc for i386 targets

---
+++ libc/arch/i386/crtn.S
@@ -0,0 +1,10 @@
+/* i386 crtn.s */
+.section .init
+	/* gcc will nicely put the contents of crtend.o's .init section here. */
+	popl %ebp
+	ret
+
+.section .fini
+	/* gcc will nicely put the contents of crtend.o's .fini section here. */
+	popl %ebp
+	ret
