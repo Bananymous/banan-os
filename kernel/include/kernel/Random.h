3823de65529292f68b274b89a59489bf1a590ecb
Bananymous <oskari.alaranta@bananymous.com>
2024-05-29 15:32:00 +0300

    Kernel: Add templated get function for Random

--- kernel/include/kernel/Random.h
+++ kernel/include/kernel/Random.h
@@ -11,6 +11,14 @@ namespace Kernel
 		static void initialize();
 		static uint32_t get_u32();
 		static uint64_t get_u64();
+		template<typename T>
+		static T get();
 	};
+
+	template<>
+	inline uint32_t Random::get<uint32_t>() { return Random::get_u32(); }
+
+	template<>
+	inline uint64_t Random::get<uint64_t>() { return Random::get_u64(); }
 
 }
