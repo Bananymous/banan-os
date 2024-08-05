dbba9128a441ddeb2f3fd1751e0469cbaf04f92b
Bananymous <oskari.alaranta@bananymous.com>
2024-08-05 15:16:40 +0300

    Ports: Add ncurses and vim port!
    vim is kind of buggy because my virtual tty {insert,delete} line ansi
    codes don't really work.

---
+++ ports/ncurses/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash ../install.sh
+
+NAME='ncurses'
+VERSION='6.5'
+DOWNLOAD_URL="https://invisible-island.net/archives/ncurses/ncurses-$VERSION.tar.gz#136d91bc269a9a5785e5f9e980bc76ab57428f604ce3e5a5a90cebc767971cc6"
+CONFIGURE_OPTIONS=(
+	'--disable-db-intall'
+	'--disable-widec'
+	'--without-ada'
+	'--without-manpages'
+	'--without-dlsym'
+	'--without-cxx-binding'
+)
---
+++ ports/vim/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash ../install.sh
+
+NAME='vim'
+VERSION='9.0'
+DOWNLOAD_URL="ftp://ftp.vim.org/pub/vim/unix/vim-$VERSION.tar.bz2#a6456bc154999d83d0c20d968ac7ba6e7df0d02f3cb6427fb248660bacfb336e"
+TAR_CONTENT='vim90'
+DEPENDENCIES=( 'ncurses' )
+CONFIGURE_OPTIONS=(
+	'--with-tlib=ncurses'
+	'--disable-nls'
+	'--disable-sysmouse'
+	'--disable-channel'
+	'vim_cv_toupper_broken=no'
+	'vim_cv_terminfo=yes'
+	'vim_cv_tgetent=yes'
+	'vim_cv_getcwd_broken=no'
+	'vim_cv_stat_ignores_slash=yes'
+	'vim_cv_memmove_handles_overlap=yes'
+)
