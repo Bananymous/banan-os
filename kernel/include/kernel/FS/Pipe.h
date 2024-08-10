bac3219a01007b4d21556c21f515b6b58e5f9cf7
Bananymous <oskari.alaranta@bananymous.com>
2024-08-10 18:16:27 +0300

    Kernel: Fix `Pipe::can_read_impl()`
    The logic was inversed, which made all select calls report incorrectly
    for pipes. This made terminal emulator just freeze.

--- kernel/include/kernel/FS/Pipe.h
+++ kernel/include/kernel/FS/Pipe.h
@@ -34,7 +34,7 @@ namespace Kernel
 		virtual BAN::ErrorOr<size_t> read_impl(off_t, BAN::ByteSpan) override;
 		virtual BAN::ErrorOr<size_t> write_impl(off_t, BAN::ConstByteSpan) override;
 
-		virtual bool can_read_impl() const override { return m_buffer_size == 0; }
+		virtual bool can_read_impl() const override { return m_buffer_size > 0; }
 		virtual bool can_write_impl() const override { return true; }
 		virtual bool has_error_impl() const override { return false; }
 
