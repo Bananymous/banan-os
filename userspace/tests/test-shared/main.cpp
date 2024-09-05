6affef76b1ba1d7d15d3b43cc11b520b80c208e3
Bananymous <oskari.alaranta@bananymous.com>
2024-09-05 12:59:48 +0300

    test-shared: Add more shared tests

--- userspace/tests/test-shared/main.cpp
+++ userspace/tests/test-shared/main.cpp
@@ -1,6 +1,11 @@
+#include <errno.h>
 #include <stdio.h>
 
 int main()
 {
-	printf("printf works!\n");
+	printf("printf works\n");
+	fprintf(stdout, "fprintf(stdout) works!\n");
+
+	errno = ENOTSUP;
+	perror(nullptr);
 }
