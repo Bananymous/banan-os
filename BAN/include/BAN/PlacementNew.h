df4f37d68d7c1a35918a6c4ecc465f06e8b44e6b
Bananymous <oskari.alaranta@bananymous.com>
2024-10-10 21:55:25 +0300

    BAN: only define placement new operators for banan-os targets
    This allows building and using BAN library outside of banan-os!

--- BAN/include/BAN/PlacementNew.h
+++ BAN/include/BAN/PlacementNew.h
@@ -2,5 +2,9 @@
 
 #include <stddef.h>
 
+#ifdef __banan_os__
 inline void* operator new(size_t, void* addr)	{ return addr; }
 inline void* operator new[](size_t, void* addr)	{ return addr; }
+#else
+#include <new>
+#endif
