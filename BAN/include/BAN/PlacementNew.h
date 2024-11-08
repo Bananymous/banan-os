4417268ecc93f670100ab7485ffde7c0eb8f4afb
Bananymous <oskari.alaranta@bananymous.com>
2024-11-08 02:48:07 +0200

    BAN: only include <new> in PlacementNew.h
    I was getting annoying multiple definitions for placement new and my
    build system requires that c++ libraries are compiled. I guess this is
    the easiest and cleanest solution

--- BAN/include/BAN/PlacementNew.h
+++ BAN/include/BAN/PlacementNew.h
@@ -1,10 +1,3 @@
 #pragma once
 
-#include <stddef.h>
-
-#ifdef __banan_os__
-inline void* operator new(size_t, void* addr)	{ return addr; }
-inline void* operator new[](size_t, void* addr)	{ return addr; }
-#else
 #include <new>
-#endif
