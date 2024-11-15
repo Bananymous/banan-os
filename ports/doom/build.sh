51bfe4252d416f4dc9806a776bd6ef05f6f3b3cc
Bananymous <oskari.alaranta@bananymous.com>
2024-11-15 20:50:40 +0200

    ports/doom: download doom1.wad to common directory between archs

--- ports/doom/build.sh
+++ ports/doom/build.sh
@@ -9,13 +9,13 @@ configure() {
 }
 
 build() {
-	if [ ! -f doom1.wad ]; then
-		wget https://distro.ibiblio.org/slitaz/sources/packages/d/doom1.wad || exit 1
+	if [ ! -f ../doom1.wad ]; then
+		wget https://distro.ibiblio.org/slitaz/sources/packages/d/doom1.wad -O ../doom1.wad || exit 1
 	fi
 	make --directory doomgeneric --file Makefile.banan_os -j$(nproc) || exit 1
 }
 
 install() {
 	cp doomgeneric/build/doom "${BANAN_SYSROOT}/bin/" || exit 1
-	cp ./doom1.wad "$BANAN_SYSROOT/home/user/" || exit 1
+	cp ../doom1.wad "$BANAN_SYSROOT/home/user/" || exit 1
 }
