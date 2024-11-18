f8ef11b4d25203e2a5b5fac770e9f0bb9829ec5e
Bananymous <oskari.alaranta@bananymous.com>
2024-11-19 00:16:44 +0200

    BAN: Make PlacementNew.h only include <new> if it exists
    Otherwise it will define the functions itself. This allows compiling
    toolchain as stdlibc++ depends on BAN.

--- BAN/include/BAN/PlacementNew.h
+++ BAN/include/BAN/PlacementNew.h
@@ -1,3 +1,10 @@
 #pragma once
 
-#include <new>
+#if __has_include(<new>)
+	#include <new>
+#else
+	#include <stddef.h>
+
+	inline void* operator new(size_t, void* addr) { return addr; }
+	inline void* operator new[](size_t, void* addr) { return addr; }
+#endif
