11cac7a646ac6b5235320fd8fec4573be05b4e36
Bananymous <oskari.alaranta@bananymous.com>
2024-12-01 02:47:01 +0200

    aoc2024: prepare programming environment

---
+++ userspace/aoc2024/day-template/main.cpp
@@ -0,0 +1,47 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+i64 part1(FILE* fp)
+{
+	(void)fp;
+	return -1;
+}
+
+i64 part2(FILE* fp)
+{
+	(void)fp;
+	return -1;
+}
+
+int main(int argc, char** argv)
+{
+	const char* file_path = "/usr/share/aoc2024/day-template_input.txt";
+
+	if (argc >= 2)
+		file_path = argv[1];
+
+	FILE* fp = fopen(file_path, "r");
+	if (fp == nullptr)
+	{
+		perror("fopen");
+		return 1;
+	}
+
+	printf("part1: %" PRId64 "\n", part1(fp));
+
+	fseek(fp, 0, SEEK_SET);
+
+	printf("part2: %" PRId64 "\n", part2(fp));
+
+	fclose(fp);
+}
---
+++ userspace/aoc2024/full/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main()
+{
+	for (int i = 1; i <= 25; i++)
+	{
+		char command[128];
+		sprintf(command, "/bin/aoc2024/day%d", i);
+
+		struct stat st;
+		if (stat(command, &st) == -1)
+			continue;
+
+		printf("day%d\n", i);
+
+		system(command);
+	}
+
+	return 0;
+}
