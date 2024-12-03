176693dd5a52d1574d5420547137508014232ca4
Bananymous <oskari.alaranta@bananymous.com>
2024-12-03 16:21:54 +0200

    LibC: Implement wcscmp and wcsncmp

--- userspace/libraries/LibC/wchar.cpp
+++ userspace/libraries/LibC/wchar.cpp
@@ -6,3 +6,21 @@ size_t mbrtowc(wchar_t* __restrict, const char* __restrict, size_t, mbstate_t* _
 {
 	ASSERT_NOT_REACHED();
 }
+
+int wcscmp(const wchar_t* ws1, const wchar_t* ws2)
+{
+	for (; *ws1 && *ws2; ws1++, ws2++)
+		if (*ws1 != *ws2)
+			break;
+	return *ws1 - *ws2;
+}
+
+int wcsncmp(const wchar_t* ws1, const wchar_t* ws2, size_t n)
+{
+	if (n == 0)
+		return 0;
+	for (; --n && *ws1 && *ws2; ws1++, ws2++)
+		if (*ws1 != *ws2)
+			break;
+	return *ws1 - *ws2;
+}
