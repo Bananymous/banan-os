62bee04fc03af0dee77683745e5c27cfa5255ceb
Bananymous <oskari.alaranta@bananymous.com>
2024-12-03 16:21:18 +0200

    LibC: Implement dummy utime that just checks file existance

--- userspace/libraries/LibC/utime.cpp
+++ userspace/libraries/LibC/utime.cpp
@@ -1,8 +1,11 @@
-#include <BAN/Assert.h>
-
 #include <utime.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
-int utime(const char*, const struct utimbuf*)
+int utime(const char* filename, const struct utimbuf* times)
 {
-	ASSERT_NOT_REACHED();
+	fprintf(stddbg, "TODO: utime(\"%s\", %p)\n", filename, times);
+
+	struct stat st;
+	return stat(filename, &st);
 }
