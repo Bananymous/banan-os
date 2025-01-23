58cdcf754c0e7b58ef7dcca608af5de222d2eaa3
Bananymous <oskari.alaranta@bananymous.com>
2025-01-23 21:58:29 +0200

    ports: Update curl port
    This patch updates curl to version 8.11.1 and adds support for dynamic
    libraries

--- ports/curl/build.sh
+++ ports/curl/build.sh
@@ -1,21 +1,26 @@
 #!/bin/bash ../install.sh
 
 NAME='curl'
-VERSION='8.8.0'
-DOWNLOAD_URL="https://curl.se/download/curl-$VERSION.tar.gz#77c0e1cd35ab5b45b659645a93b46d660224d0024f1185e8a95cdb27ae3d787d"
-DEPENDENCIES=('ca-certificates' 'openssl' 'zlib')
+VERSION='8.11.1'
+DOWNLOAD_URL="https://curl.se/download/curl-$VERSION.tar.gz#a889ac9dbba3644271bd9d1302b5c22a088893719b72be3487bc3d401e5c4e80"
+DEPENDENCIES=('ca-certificates' 'openssl' 'zlib' 'zstd')
 CONFIGURE_OPTIONS=(
 	'--disable-threaded-resolver'
 	'--disable-ipv6'
 	'--disable-docs'
 	'--disable-ntlm'
+	'--enable-optimize'
 	'--with-openssl'
 	'--with-zlib'
+	'--with-zstd'
+	'--without-libpsl'
 	'--with-ca-bundle=/etc/ssl/certs/ca-certificates.crt'
 	'--without-ca-path'
 )
 
 install() {
 	make install DESTDIR="$BANAN_SYSROOT" || exit 1
+
+	# remove libtool file
 	rm -f $BANAN_SYSROOT/usr/lib/libcurl.la
 }
