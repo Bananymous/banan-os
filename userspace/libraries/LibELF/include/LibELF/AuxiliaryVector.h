b41868333764bfe56ad13a13a30c52f0fa20a7c9
Bananymous <oskari.alaranta@bananymous.com>
2025-04-15 21:31:02 +0300

    LibELF: Add definition and values for auxiliary vectors

---
+++ userspace/libraries/LibELF/include/LibELF/AuxiliaryVector.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace LibELF
+{
+
+	struct AuxiliaryVector
+	{
+		uint32_t a_type;
+		union
+		{
+			uint32_t a_val;
+			void* a_ptr;
+		} a_un;
+	};
+
+	enum AuxiliaryVectorValues
+	{
+		AT_NULL   = 0,
+		AT_IGNORE = 1,
+		AT_EXECFD = 2,
+		AT_PHDR   = 3,
+		AT_PHENT  = 4,
+		AT_PHNUM  = 5,
+	};
+
+}
