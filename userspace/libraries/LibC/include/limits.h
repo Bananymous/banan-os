16dbfbb267af38596127772c587a922e0e0c357f
Bananymous <oskari.alaranta@bananymous.com>
2025-04-20 17:15:20 +0300

    LibC: Make limits.h `*_MAX` usable in #if comparisons

--- userspace/libraries/LibC/include/limits.h
+++ userspace/libraries/LibC/include/limits.h
@@ -72,22 +72,25 @@ __BEGIN_DECLS
 #define CHAR_MAX SCHAR_MAX
 #define CHAR_MIN SCHAR_MIN
 
-#define SCHAR_MAX 127
+#define SCHAR_MAX __SCHAR_MAX__
 #define SHRT_MAX __SHRT_MAX__
 #define INT_MAX __INT_MAX__
 #define LONG_MAX __LONG_MAX__
+#define LLONG_MAX __LONG_LONG_MAX__
 #define SSIZE_MAX __PTRDIFF_MAX__
 
 #define SCHAR_MIN (-SCHAR_MAX - 1)
 #define SHRT_MIN (-SHRT_MAX - 1)
 #define INT_MIN (-INT_MAX - 1)
 #define LONG_MIN (-LONG_MAX - 1)
+#define LLONG_MIN (-LLONG_MAX - 1)
 #define SSIZE_MIN (-SSIZE_MAX - 1)
 
-#define USCHAR_MAX ((unsigned char)2 * SCHAR_MAX + 1)
-#define USHRT_MAX ((unsigned short)2 * SHRT_MAX + 1)
-#define UINT_MAX ((unsigned int)2 * INT_MAX + 1)
-#define ULONG_MAX ((unsigned long)2 * LONG_MAX + 1)
+#define USCHAR_MAX (SCHAR_MAX * 2 + 1)
+#define USHRT_MAX (SHRT_MAX * 2 + 1)
+#define UINT_MAX (INT_MAX * 2U + 1)
+#define ULONG_MAX (LONG_MAX * 2UL + 1)
+#define ULLONG_MAX (LLONG_MAX * 2ULL + 1)
 
 __END_DECLS
 
