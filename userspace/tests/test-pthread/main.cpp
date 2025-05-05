2dc4733ac15e56bd3318522d93126b1fe1261dbe
Bananymous <oskari.alaranta@bananymous.com>
2025-05-05 19:21:47 +0300

    test: improve pthread test

--- userspace/tests/test-pthread/main.cpp
+++ userspace/tests/test-pthread/main.cpp
@@ -1,28 +1,61 @@
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+pthread_spinlock_t spinlock;
 
 void* thread_func(void*)
 {
-	printf("hello from thread\n");
-	return nullptr;
+	printf("[THREAD] locking spinlock\n");
+	pthread_spin_lock(&spinlock);
+	printf("[THREAD] got spinlock\n");
+
+	sleep(1);
+
+	printf("[THREAD] releasing spinlock\n");
+	pthread_spin_unlock(&spinlock);
+
+	int* value = static_cast<int*>(malloc(sizeof(int)));
+	if (value == nullptr)
+	{
+		perror("malloc");
+		return nullptr;
+	}
+
+	*value = 69;
+
+	printf("[THREAD] exiting with %d\n", *value);
+
+	return value;
 }
 
 int main(int argc, char** argv)
 {
-	pthread_t tid;
+	pthread_spin_init(&spinlock, 0);
 
-	printf("creating thread\n");
+	printf("[MAIN] locking spinlock\n");
+	pthread_spin_lock(&spinlock);
 
-	if (pthread_create(&tid, nullptr, &thread_func, nullptr) == -1)
-	{
-		perror("pthread_create");
-		return 1;
-	}
+	printf("[MAIN] creating thread\n");
+
+	pthread_t thread;
+	pthread_create(&thread, nullptr, &thread_func, nullptr);
 
 	sleep(1);
 
-	printf("exiting\n");
+	printf("[MAIN] releasing spinlock\n");
+	pthread_spin_unlock(&spinlock);
+
+	printf("[MAIN] joining thread\n");
+
+	void* value;
+	pthread_join(thread, &value);
+
+	if (value == nullptr)
+		printf("[MAIN] thread returned NULL\n");
+	else
+		printf("[MAIN] thread returned %d\n", *static_cast<int*>(value));
 
 	return 0;
 }
