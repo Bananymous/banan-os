8574fcf6e12411592e99bce91dfaa89e0a187291
Bananymous <oskari.alaranta@bananymous.com>
2025-06-11 16:01:19 +0300

    ports: Add libwebp port

---
+++ ports/libwebp/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash ../install.sh
+
+NAME='libwebp'
+VERSION='1.5.0'
+DOWNLOAD_URL="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-$VERSION.tar.gz#7d6fab70cf844bf6769077bd5d7a74893f8ffd4dfb42861745750c63c2a5c92c"
+DEPENDENCIES=('libpng' 'libjpeg' 'libtiff')
+CONFIGURE_OPTIONS=(
+	"--with-pngincludedir=$BANAN_SYSROOT/usr/include"
+	"--with-pnglibdir=$BANAN_SYSROOT/usr/lib"
+)
+
+install() {
+	make install DESTDIR="$BANAN_SYSROOT" || exit 1
+
+	# remove libtool files
+	rm -f $BANAN_SYSROOT/usr/lib/libwebp.la
+	rm -f $BANAN_SYSROOT/usr/lib/libwebpdemux.la
+	rm -f $BANAN_SYSROOT/usr/lib/libwebpmux.la
+}
