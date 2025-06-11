f4f424bf04867ca184e0d073eb3e6eb02ed52484
Bananymous <oskari.alaranta@bananymous.com>
2025-06-11 16:01:02 +0300

    ports: Add libtiff port

---
+++ ports/libtiff/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash ../install.sh
+
+NAME='libtiff'
+VERSION='4.7.0'
+DOWNLOAD_URL="https://download.osgeo.org/libtiff/tiff-$VERSION.tar.gz#67160e3457365ab96c5b3286a0903aa6e78bdc44c4bc737d2e486bcecb6ba976"
+TAR_CONTENT="tiff-$VERSION"
+DEPENDENCIES=('zlib' 'zstd' 'libjpeg')
+
+install() {
+	make install "DESTDIR=$BANAN_SYSROOT" || exit 1
+
+	# remove libtool files
+	rm -f $BANAN_SYSROOT/usr/lib/libtiff.la
+	rm -f $BANAN_SYSROOT/usr/lib/libtiffxx.la
+}
