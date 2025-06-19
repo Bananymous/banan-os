7dc187fdb152fd10ff5e24d0c06abda3e5c1b5fa
Bananymous <oskari.alaranta@bananymous.com>
2025-06-19 19:39:37 +0300

    ports: Update gcc 12.2.0->15.1.0
    Also enable threading :)

--- ports/gcc/build.sh
+++ ports/gcc/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash ../install.sh
 
 NAME='gcc'
-VERSION='12.2.0'
-DOWNLOAD_URL="https://ftp.gnu.org/gnu/gcc/gcc-$VERSION/gcc-$VERSION.tar.gz#ac6b317eb4d25444d87cf29c0d141dedc1323a1833ec9995211b13e1a851261c"
+VERSION='15.1.0'
+DOWNLOAD_URL="https://ftp.gnu.org/gnu/gcc/gcc-$VERSION/gcc-$VERSION.tar.gz#51b9919ea69c980d7a381db95d4be27edf73b21254eb13d752a08003b4d013b1"
 DEPENDENCIES=('binutils' 'gmp' 'mpfr' 'mpc')
 CONFIGURE_OPTIONS=(
 	"--target=$BANAN_TOOLCHAIN_TRIPLE"
 	'--with-sysroot=/'
 	"--with-build-sysroot=$BANAN_SYSROOT"
 	'--enable-initfini-array'
+	'--enable-threads=posix'
 	'--enable-shared'
 	'--enable-lto'
 	'--disable-nls'
