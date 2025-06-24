fe804c123d1ce42135b1380c245b5c40fe1ef728
Bananymous <oskari.alaranta@bananymous.com>
2025-06-24 23:07:16 +0300

    ports: Add expat port

---
+++ ports/expat/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash ../install.sh
+
+NAME='expat'
+VERSION='2.7.1'
+DOWNLOAD_URL="https://github.com/libexpat/libexpat/releases/download/R_2_7_1/expat-$VERSION.tar.gz#0cce2e6e69b327fc607b8ff264f4b66bdf71ead55a87ffd5f3143f535f15cfa2"
