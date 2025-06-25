85f200bd86bb5c037bb8e946982174789124c174
Bananymous <oskari.alaranta@bananymous.com>
2025-06-25 10:40:24 +0300

    LibC: Add sched_get_priority_{min,max}
    I don't support priority scheduling so these are just no-ops

--- userspace/libraries/LibC/sched.cpp
+++ userspace/libraries/LibC/sched.cpp
@@ -2,6 +2,18 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+int sched_get_priority_max(int policy)
+{
+	(void)policy;
+	return 0;
+}
+
+int sched_get_priority_min(int policy)
+{
+	(void)policy;
+	return 0;
+}
+
 int sched_yield(void)
 {
 	return syscall(SYS_YIELD);
