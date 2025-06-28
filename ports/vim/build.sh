0a3c10566b147883b7cab910180930d6a8880d05
Bananymous <oskari.alaranta@bananymous.com>
2025-06-28 04:04:21 +0300

    ports: vim update to 9.1

--- ports/vim/build.sh
+++ ports/vim/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash ../install.sh
 
 NAME='vim'
-VERSION='9.0'
-DOWNLOAD_URL="ftp://ftp.vim.org/pub/vim/unix/vim-$VERSION.tar.bz2#a6456bc154999d83d0c20d968ac7ba6e7df0d02f3cb6427fb248660bacfb336e"
-TAR_CONTENT='vim90'
+VERSION='9.1.1485'
+DOWNLOAD_URL="https://github.com/vim/vim/archive/refs/tags/v$VERSION.tar.gz#89b48e30c9e97bb819ffed752c8a1727b70bed79890bffe9da5f7c2170487dd2"
 DEPENDENCIES=('ncurses')
 CONFIGURE_OPTIONS=(
 	'--with-tlib=ncurses'
@@ -18,9 +17,19 @@ CONFIGURE_OPTIONS=(
 	'vim_cv_memmove_handles_overlap=yes'
 )
 
-install() {
-	make install "DESTDIR=$BANAN_SYSROOT" || exit 1
+post_configure() {
+	# vim doesn't do link tests, so it thinks these exists
+	config_undefines=(
+		'HAVE_SHM_OPEN'
+		'HAVE_TIMER_CREATE'
+	)
 
+	for undefine in "${config_undefines[@]}"; do
+		sed -i "s|^#define $undefine 1$|/\* #undef $undefine \*/|" src/auto/config.h
+	done
+}
+
+post_install() {
 	shellrc="$BANAN_SYSROOT/home/user/.shellrc"
 	grep -q 'export EDITOR=' "$shellrc" || echo 'export EDITOR=vim' >> "$shellrc"
 }
