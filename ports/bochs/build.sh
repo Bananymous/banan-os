fe9a8b542ad8990721f378776dac58bfd6486a04
Bananymous <oskari.alaranta@bananymous.com>
2025-06-28 20:52:52 +0300

    ports: Add bochs port
    THIS IS SUPER SLOW

---
+++ ports/bochs/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash ../install.sh
+
+NAME='bochs'
+VERSION='3.0'
+DOWNLOAD_URL="https://sourceforge.net/projects/bochs/files/bochs/$VERSION/bochs-$VERSION.tar.gz#cb6f542b51f35a2cc9206b2a980db5602b7cd1b7cf2e4ed4f116acd5507781aa"
+DEPENDENCIES=('SDL2')
+CONFIGURE_OPTIONS=(
+	'--with-sdl2'
+	'--enable-x86-64'
+)
