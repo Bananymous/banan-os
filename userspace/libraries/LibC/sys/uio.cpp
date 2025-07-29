66fe2f2e50571a6eeb3d1da3d80b3e895828279e
Bananymous <oskari.alaranta@bananymous.com>
2025-07-30 00:06:50 +0300

    LibC: Fix readv and writev

--- userspace/libraries/LibC/sys/uio.cpp
+++ userspace/libraries/LibC/sys/uio.cpp
@@ -9,7 +9,7 @@ ssize_t readv(int fildes, const struct iovec* iov, int iovcnt)
 	size_t result = 0;
 	for (int i = 0; i < iovcnt; i++)
 	{
-		uint8_t* base = static_cast<uint8_t*>(iov->iov_base);
+		uint8_t* base = static_cast<uint8_t*>(iov[i].iov_base);
 
 		size_t nread = 0;
 		while (nread < iov[i].iov_len)
@@ -33,7 +33,7 @@ ssize_t writev(int fildes, const struct iovec* iov, int iovcnt)
 	size_t result = 0;
 	for (int i = 0; i < iovcnt; i++)
 	{
-		const uint8_t* base = static_cast<const uint8_t*>(iov->iov_base);
+		const uint8_t* base = static_cast<const uint8_t*>(iov[i].iov_base);
 
 		size_t nwrite = 0;
 		while (nwrite < iov[i].iov_len)
