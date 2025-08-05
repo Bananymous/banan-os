bd885a01e5bdf38c03ab12f6046bf7d36cbf6fc2
Bananymous <oskari.alaranta@bananymous.com>
2025-08-05 03:47:52 +0300

    ports/expat: Remove libtool file

--- ports/expat/build.sh
+++ ports/expat/build.sh
@@ -4,3 +4,8 @@ NAME='expat'
 VERSION='2.7.1'
 DOWNLOAD_URL="https://github.com/libexpat/libexpat/releases/download/R_2_7_1/expat-$VERSION.tar.gz#0cce2e6e69b327fc607b8ff264f4b66bdf71ead55a87ffd5f3143f535f15cfa2"
 CONFIG_SUB=('conftools/config.sub')
+
+post_install() {
+	# remove libtool file
+	rm -f $BANAN_SYSROOT/usr/lib/libexpat.la
+}
