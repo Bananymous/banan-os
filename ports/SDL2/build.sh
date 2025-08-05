c7f89c9b777bce22d4fee0420d993196834bb7c9
Bananymous <oskari.alaranta@bananymous.com>
2025-08-06 02:28:33 +0300

    ports: Update SDL2 2.30.11 -> 2.32.8

--- ports/SDL2/build.sh
+++ ports/SDL2/build.sh
@@ -1,25 +1,22 @@
 #!/bin/bash ../install.sh
 
 NAME='SDL2'
-VERSION='2.30.11'
-DOWNLOAD_URL="https://github.com/libsdl-org/SDL/archive/refs/tags/release-$VERSION.tar.gz#cc6136dd964854e8846c679703322f3e2a341d27a06a53f8b3f642c26f1b0cfd"
-TAR_CONTENT="SDL-release-$VERSION"
+VERSION='2.32.8'
+DOWNLOAD_URL="https://github.com/libsdl-org/SDL/releases/download/release-$VERSION/SDL2-$VERSION.tar.gz#0ca83e9c9b31e18288c7ec811108e58bac1f1bb5ec6577ad386830eac51c787e"
 DEPENDENCIES=('mesa')
 
 configure() {
-	$BANAN_CMAKE \
+	$BANAN_CMAKE --fresh -S . -B build -G Ninja \
 		--toolchain="$BANAN_TOOLCHAIN_DIR/Toolchain.txt" \
-		--fresh -GNinja -S . -B build \
 		-DCMAKE_INSTALL_PREFIX='/usr' \
 		-DCMAKE_BUILD_TYPE=Release \
-		-DBANAN_OS=true \
-		-DUNIX=true \
 		-DSDL_LIBSAMPLERATE=OFF \
-		-DSDL_PTHREADS_SEM=OFF
+		-DSDL_PTHREADS_SEM=OFF \
+		 || exit 1
 }
 
 build() {
-	$BANAN_CMAKE --build build --config Release || exit 1
+	$BANAN_CMAKE --build build || exit 1
 }
 
 install() {
