f61c78efd544e72fda9ed4560c8a5e07c2c4c3ef
Bananymous <oskari.alaranta@bananymous.com>
2025-08-07 03:08:01 +0300

    ports/tinygb: Use SDL2 instead of our own frontend

--- ports/tinygb/build.sh
+++ ports/tinygb/build.sh
@@ -3,13 +3,15 @@
 NAME='tinygb'
 VERSION='git'
 DOWNLOAD_URL="https://github.com/jewelcodes/tinygb.git#57fdaff675a6b5b963b2b6624868d9698eabe375"
+DEPENDENCIES=('SDL2')
 
 configure() {
-	make -f Makefile.banan_os clean
+	sed -i "s|shell sdl2-config|shell $BANAN_SYSROOT/usr/bin/sdl2-config|g" Makefile
+	make clean
 }
 
 build() {
-	make -f Makefile.banan_os -j$(nproc) || exit 1
+	make CC="$CC" LD="$CC" || exit 1
 }
 
 install() {
