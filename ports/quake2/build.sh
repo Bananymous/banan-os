0b81bb9b10cc57dd323be6da6fbb8b8ad9984bc2
Bananymous <oskari.alaranta@bananymous.com>
2025-08-10 19:56:52 +0300

    ports/quake2: Use SDL2 instead of our own frontend

--- ports/quake2/build.sh
+++ ports/quake2/build.sh
@@ -2,10 +2,11 @@
 
 NAME='quake2'
 VERSION='git'
-DOWNLOAD_URL="https://github.com/ozkl/quake2generic.git#a967e4f567a98941326fc7fe76eee5e52a04a633"
+DOWNLOAD_URL="https://github.com/ozkl/quake2generic.git#50190797664fd42fc1b0266150c54f76f92bfa15"
+DEPENDENCIES=('SDL2' 'SDL2_mixer')
 
 configure() {
-	:
+	make clean
 }
 
 build() {
@@ -24,11 +25,13 @@ build() {
 		echo "File hash does not match" >&2
 		exit 1
 	fi
-	make -j$(nproc) || exit 1
+
+	cflags='-Dstricmp=strcasecmp -Wno-incompatible-pointer-types'
+	make CC="$CC" BASE_CFLAGS="$cflags" SDL_PATH="$BANAN_SYSROOT/usr/bin/" -j$(nproc) || exit 1
 }
 
 install() {
-	cp build/quake2 "${BANAN_SYSROOT}/bin/" || exit 1
+	cp -v build/quake2-soft "${BANAN_SYSROOT}/bin/quake2" || exit 1
 
 	baseq2_tar=$(realpath ../baseq2.tar.gz || exit 1)
 	cd "$BANAN_SYSROOT/home/user/"
