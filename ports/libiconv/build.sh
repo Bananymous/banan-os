59fff26a5f7cee6b7491a28108ff9846230b0602
Bananymous <oskari.alaranta@bananymous.com>
2025-08-11 18:46:15 +0300

    LibC: Remove our iconv.h header and add libiconv port
    If I ever end up writing my own iconv, i will drop the port :D

---
+++ ports/libiconv/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash ../install.sh
+
+NAME='libiconv'
+VERSION='1.18'
+DOWNLOAD_URL="https://ftp.gnu.org/pub/gnu/libiconv/libiconv-$VERSION.tar.gz#3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8"
+CONFIG_SUB=('build-aux/config.sub' 'libcharset/build-aux/config.sub')
+CONFIGURE_OPTIONS=(
+	'--disable-nls'
+	'CFLAGS=-std=c11'
+)
+
+pre_configure() {
+	echo '#include_next <sys/types.h>' > srclib/sys_types.in.h
+}
+
+post_install() {
+	# remove libtool file
+	rm -f $BANAN_SYSROOT/usr/lib/libcharset.la
+	rm -f $BANAN_SYSROOT/usr/lib/libiconv.la
+}
