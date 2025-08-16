70bbdbd8f5683708bde8563e6b6381093c68dc30
Bananymous <oskari.alaranta@bananymous.com>
2025-08-16 22:56:03 +0300

    LibC: Cleanup syslog output
    add ": " after the identification and formatted output. syslog does not
    require trailing newline so add it in case it is missing.

--- userspace/libraries/LibC/syslog.cpp
+++ userspace/libraries/LibC/syslog.cpp
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 #include <syslog.h>
 
 static const char* s_ident = nullptr;
@@ -20,11 +21,15 @@ void syslog(int priority, const char* format, ...)
 {
 	(void)priority;
 	if (s_ident)
-		fprintf(s_log_file, "%s", s_ident);
+		fprintf(s_log_file, "%s: ", s_ident);
 	va_list args;
 	va_start(args, format);
 	vfprintf(s_log_file, format, args);
 	va_end(args);
+
+	const size_t format_len = strlen(format);
+	if (format_len && format[format_len - 1] != '\n')
+		fputc('\n', s_log_file);
 }
 
 void closelog()
