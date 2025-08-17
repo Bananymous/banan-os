74af4e91503199946e9229c6033cebc1e4dce3bf
Bananymous <oskari.alaranta@bananymous.com>
2025-08-18 00:01:12 +0300

    ports/SDL2_mixer: Add MIDI support

--- ports/SDL2_mixer/build.sh
+++ ports/SDL2_mixer/build.sh
@@ -3,15 +3,16 @@
 NAME='SDL2_mixer'
 VERSION='2.8.1'
 DOWNLOAD_URL="https://github.com/libsdl-org/SDL_mixer/releases/download/release-$VERSION/SDL2_mixer-$VERSION.tar.gz#cb760211b056bfe44f4a1e180cc7cb201137e4d1572f2002cc1be728efd22660"
-DEPENDENCIES=('SDL2')
+DEPENDENCIES=('SDL2' 'timidity')
 
 configure() {
 	$BANAN_CMAKE --fresh -S . -B build -G Ninja \
 		--toolchain="$BANAN_TOOLCHAIN_DIR/Toolchain.txt" \
 		-DCMAKE_INSTALL_PREFIX='/usr' \
 		-DCMAKE_BUILD_TYPE=Release \
+		-DSDL2MIXER_DEPS_SHARED=OFF \
+		-DSDL2MIXER_MIDI_FLUIDSYNTH=OFF \
 		-DSDL2MIXER_WAVPACK=OFF \
-		-DSDL2MIXER_MIDI=OFF \
 		-DSDL2MIXER_OPUS=OFF \
 		-DSDL2MIXER_MOD=OFF \
 		 || exit 1
