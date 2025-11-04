bdf44235126a9862bb4f2911f5655bccc2016eea
Bananymous <oskari.alaranta@bananymous.com>
2025-11-04 23:06:13 +0200

    ports/freetype: Add missing dependencies

--- ports/freetype/build.sh
+++ ports/freetype/build.sh
@@ -3,6 +3,7 @@
 NAME='freetype'
 VERSION='2.13.3'
 DOWNLOAD_URL="https://download.savannah.gnu.org/releases/freetype/freetype-$VERSION.tar.gz#5c3a8e78f7b24c20b25b54ee575d6daa40007a5f4eea2845861c3409b3021747"
+DEPENDENCIES=('zlib' 'libpng')
 CONFIG_SUB=('builds/unix/config.sub')
 
 CONFIGURE_OPTIONS=(
