998ea25fb9b42d0c8fc713b19faf45aaa280ddd2
Bananymous <oskari.alaranta@bananymous.com>
2025-11-09 23:54:30 +0200

    LibC: Add netinet/in_systm.h compatibility header

---
+++ userspace/libraries/LibC/include/netinet/in_systm.h
@@ -0,0 +1,16 @@
+#ifndef _NETINET_IN_SYSTM_H
+#define _NETINET_IN_SYSTM_H 1
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+#include <stdint.h>
+
+typedef uint16_t n_short;
+typedef uint32_t n_long;
+typedef uint32_t n_time;
+
+__END_DECLS
+
+#endif
