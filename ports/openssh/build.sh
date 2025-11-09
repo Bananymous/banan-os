82c8eeb4beec5ac77d26b751950afced2c32d1fd
Bananymous <oskari.alaranta@bananymous.com>
2025-11-10 00:48:46 +0200

    ports/openssh: Update to 10.2p1 and enable fd passing

--- ports/openssh/build.sh
+++ ports/openssh/build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash ../install.sh
 
 NAME='openssh'
-VERSION='10.0p1'
-DOWNLOAD_URL="https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$VERSION.tar.gz#021a2e709a0edf4250b1256bd5a9e500411a90dddabea830ed59cef90eb9d85c"
+VERSION='10.2p1'
+DOWNLOAD_URL="https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$VERSION.tar.gz#ccc42c0419937959263fa1dbd16dafc18c56b984c03562d2937ce56a60f798b2"
 DEPENDENCIES=('zlib' 'openssl')
 CONFIG_SUB=('config.sub')
 MAKE_INSTALL_TARGETS=('install-nokeys')
 CONFIGURE_OPTIONS=(
 	'--sysconfdir=/etc'
 	'--sbindir=/usr/bin'
-	'--disable-fd-passing'
 	'CFLAGS=-Wno-deprecated-declarations'
 )
 
