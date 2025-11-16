6c4cd0d8cb1a52fc030e2c5a1c7c8d019ba94c0c
Bananymous <oskari.alaranta@bananymous.com>
2025-11-16 03:46:39 +0200

    ports: Add libmikmod port

---
+++ ports/libmikmod/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash ../install.sh
+
+NAME='libmikmod'
+VERSION='3.3.13'
+DOWNLOAD_URL="https://sourceforge.net/projects/mikmod/files/libmikmod/$VERSION/libmikmod-$VERSION.tar.gz#9fc1799f7ea6a95c7c5882de98be85fc7d20ba0a4a6fcacae11c8c6b382bb207"
+CONFIG_SUB=('autotools/config.sub')
