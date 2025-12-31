bd50444d06f0404a83f4d8a176b2dc1fc46b4df0
Bananymous <oskari.alaranta@bananymous.com>
2025-12-31 18:56:30 +0200

    ports: Update ca-certificates 2024-07-02 -> 2025-12-02

--- ports/ca-certificates/build.sh
+++ ports/ca-certificates/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash ../install.sh
 
 NAME='ca-certificates'
-VERSION='2024-07-02'
-DOWNLOAD_URL="https://curl.se/ca/cacert-$VERSION.pem#1bf458412568e134a4514f5e170a328d11091e071c7110955c9884ed87972ac9"
+VERSION='2025-12-02'
+DOWNLOAD_URL="https://curl.se/ca/cacert-$VERSION.pem#f1407d974c5ed87d544bd931a278232e13925177e239fca370619aba63c757b4"
 
 configure() {
 	:
