b2d8199480edd8c833bcb5dbb053982631ce4537
Bananymous <oskari.alaranta@bananymous.com>
2026-01-08 14:09:13 +0200

    ports/openal-soft: Add SDL2 as a dependency

--- ports/openal-soft/build.sh
+++ ports/openal-soft/build.sh
@@ -3,12 +3,11 @@
 NAME='openal-soft'
 VERSION='1.24.3'
 DOWNLOAD_URL="https://github.com/kcat/openal-soft/archive/refs/tags/$VERSION.tar.gz#7e1fecdeb45e7f78722b776c5cf30bd33934b961d7fd2a11e0494e064cc631ce"
-DEPENDENCIES=('zlib' 'libsndfile')
+DEPENDENCIES=('SDL2' 'zlib' 'libsndfile')
 
 configure() {
-	$BANAN_CMAKE \
+	$BANAN_CMAKE -B build -S . -G Ninja --fresh \
 		--toolchain="$BANAN_TOOLCHAIN_DIR/Toolchain.txt" \
-		-B build -G Ninja --fresh . \
 		-DCMAKE_INSTALL_PREFIX=/usr \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DALSOFT_EXAMPLES=OFF \
