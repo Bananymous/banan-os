e60f3711f82ae82840314a76a355788f1580f973
Bananymous <oskari.alaranta@bananymous.com>
2026-01-08 14:07:45 +0200

    ports: Update openssl 3.3.1->3.6.0

--- ports/openssl/build.sh
+++ ports/openssl/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash ../install.sh
 
 NAME='openssl'
-VERSION='3.3.1'
-DOWNLOAD_URL="https://github.com/openssl/openssl/releases/download/openssl-$VERSION/openssl-$VERSION.tar.gz#777cd596284c883375a2a7a11bf5d2786fc5413255efab20c50d6ffe6d020b7e"
+VERSION='3.6.0'
+DOWNLOAD_URL="https://github.com/openssl/openssl/releases/download/openssl-$VERSION/openssl-$VERSION.tar.gz#b6a5f44b7eb69e3fa35dbf15524405b44837a481d43d81daddde3ff21fcbb8e9"
 DEPENDENCIES=('zlib')
 MAKE_INSTALL_TARGETS=('install_sw' 'install_ssldirs')
 
