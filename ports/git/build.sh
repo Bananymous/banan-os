186fa4f1a1a33966849009e2411a2e00deac42a9
Bananymous <oskari.alaranta@bananymous.com>
2026-03-23 17:35:08 +0200

    ports: Update git 2.52.0->2.53.0

--- ports/git/build.sh
+++ ports/git/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash ../install.sh
 
 NAME='git'
-VERSION='2.52.0'
-DOWNLOAD_URL="https://www.kernel.org/pub/software/scm/git/git-$VERSION.tar.xz#3cd8fee86f69a949cb610fee8cd9264e6873d07fa58411f6060b3d62729ed7c5"
-DEPENDENCIES=('zlib' 'openssl' 'curl')
+VERSION='2.53.0'
+DOWNLOAD_URL="https://www.kernel.org/pub/software/scm/git/git-$VERSION.tar.xz#5818bd7d80b061bbbdfec8a433d609dc8818a05991f731ffc4a561e2ca18c653"
+DEPENDENCIES=('zlib' 'openssl' 'curl' 'libiconv')
 CONFIGURE_OPTIONS=(
 	'--with-curl'
 	'--with-openssl'
 	ac_cv_fread_reads_directories=no
-	ac_cv_snprintf_returns_bogus=no
+	ac_cv_iconv_omits_bom=no
 	ac_cv_lib_curl_curl_global_init=yes
+	ac_cv_snprintf_returns_bogus=no
 )
