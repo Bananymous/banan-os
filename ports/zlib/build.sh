64d3a5c8b7edf48378065d604c545428efbe5ec1
Bananymous <oskari.alaranta@bananymous.com>
2026-03-23 18:54:57 +0200

    ports: Update zlib 1.3.1->1.3.2
    1.3.1 is no longer available at zlib.net

--- ports/zlib/build.sh
+++ ports/zlib/build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash ../install.sh
 
 NAME='zlib'
-VERSION='1.3.1'
-DOWNLOAD_URL="https://www.zlib.net/zlib-$VERSION.tar.gz#9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
+VERSION='1.3.2'
+DOWNLOAD_URL="https://www.zlib.net/zlib-$VERSION.tar.gz#bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16"
 
 configure() {
-	./configure --prefix=/usr --uname=banan_os
+	cmake --fresh -B build -S . -G Ninja \
+		--toolchain="$BANAN_TOOLCHAIN_DIR/Toolchain.txt" \
+		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_BUILD_TYPE=Release \
+		|| exit 1
+}
+
+build() {
+	cmake --build build || exit 1
+}
+
+install() {
+	cmake --install build || exit 1
 }
